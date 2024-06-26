bcafd5899fb07845c6e805c93a4607838f25b4fd
mayajanvier <maya.janvier@hotmail.fr>
2024-06-26 17:35:14 +0200

    ensemble dataset first version

--- processings/dataset.py
+++ processings/dataset.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from .format_data import compute_wind_speedxr, compute_wind_speed, fit_norm_along_axis
+from .format_data import compute_wind_speedxr, compute_wind_speed, fit_norm_along_axis, format_ensemble_data_EMOS
 import xarray as xr
 import pathlib
 import re
@@ -161,7 +161,7 @@ class WeatherDataset:
         self.fit_scaler()
 
     def fit_scaler(self):
-        """Compute the mean and std of the dataset for normalization."""
+        """Get all features and fit the scaler on them."""
         all_features = []
         for file, forecast_idx_in_file, valid_time, forecast_time in self.data_index:
             dataset = xr.open_dataset(file)
@@ -195,6 +195,7 @@ class WeatherDataset:
         # get features at right format without wind speed
         data = dataset.to_array().values # (n_vars, n_levels, n_lat, n_lon)
         data = data.reshape(-1, data.shape[2], data.shape[3]) # (n_levels*n_vars, n_lat, n_lon)
+        # TODO this fails when unexpected Nan, change it 
         data = data[~np.isnan(data).all(axis=(1,2))] # drop NaN of specific humidity at levels 10,50,100
         # Normalize features
         shapes = data.shape
@@ -211,6 +212,178 @@ class WeatherDataset:
         # predicted values for the target variable at 1000 hPa
         mu = torch.tensor(dataset.sel(level=1000)[self.forecast_variable].values, dtype=torch.float) # (n_lat, n_lon)
         sigma = torch.tensor(0, dtype=torch.float)
+        valid_time = str(valid_time)
+        forecast_time = str(forecast_time)
+        # get associated ground truth
+        truth = torch.tensor(truth[self.target_variable].values.T, dtype=torch.float) # (n_lat, n_lon)
+        return {
+            'mu': mu, 'sigma': sigma, 'input': features, 'truth': truth,
+            'valid_time': valid_time, "lead_time": self.lead_time_idx, "forecast_time": forecast_time
+            }   
+
+
+class WeatherEnsDataset:
+    """Data on the fly"""
+    def __init__(
+        self,
+        surface_path,
+        levels_path,
+        obs_path,
+        target_variable,
+        lead_time_idx,
+        valid_years,
+        valid_months,
+        subset
+    ):
+        self.lead_time_idx = round(lead_time_idx/7)
+        self.target_variable = target_variable
+        self.valid_months = np.arange(valid_months[0], valid_months[1] + 1)
+        self.valid_years = np.arange(valid_years[0], valid_years[1] + 1)
+        self.validation_year_begin = self.valid_years[len(self.valid_years)*9//10]
+        self.subset = subset
+
+        # Open observations
+        self.obs = xr.open_mfdataset(obs_path + '/*.nc', combine='by_coords')
+        self.latitude = self.obs.latitude.values
+        self.longitude = self.obs.longitude.values
+
+        # Build index of counts for files
+        self.data_index = []
+        for f in os.listdir(surface_path):
+            f_surface = f"{surface_path}/{f}"
+            f_mean = f"{levels_path}/mean/{f}"
+            f_std = f"{levels_path}/std/{f}"
+            data_mean = xr.open_dataset(f_mean).isel(prediction_timedelta=self.lead_time_idx) # select lead time
+
+            # Iterate over remaining time dimension
+            if self.subset == "test":
+                time_size = data_mean.sizes["time"]
+            else:
+                time_size = data_mean.sizes["forecast_time"]
+
+            for time_idx in range(time_size):
+                if self.subset == "test":
+                    forecast_time = pd.to_datetime(data_mean.isel(time=time_idx).time.values)
+                    valid_time = pd.to_datetime(data_mean.isel(time=time_idx).valid_time.values)
+                else:
+                    forecast_time = pd.to_datetime(data_mean.isel(forecast_time=time_idx).time.values)
+                    valid_time = pd.to_datetime(data_mean.isel(forecast_time=time_idx).valid_time.values)
+            
+                if valid_time.year not in self.valid_years:
+                    continue
+                elif valid_time.month not in self.valid_months:   
+                    continue
+                self.data_index.append(
+                    (f_surface, f_mean, f_std, time_idx, valid_time, forecast_time)
+                )
+        
+        # Adapt index for train or val
+        if self.subset == "train":
+            self.data_index = [x for x in self.data_index
+                if x[4].year < self.validation_year_begin 
+            ]
+        elif self.subset == "val":
+            self.data_index = [x for x in self.data_index
+                if x[4].year >= self.validation_year_begin 
+            ]
+        elif self.subset == "test":
+            pass
+        else:
+            raise ValueError("Unrecognized subset")
+        
+        # build climatology by fitting gaussians on train data, date wise, 
+        # in a netcdf file, if not done already 
+        if self.subset == "train":
+            climato_path = f"{obs_path}/climato/{self.target_variable}_{self.valid_years[0]}_{self.valid_years[-1]}_month{self.valid_months[0]}_lead{self.lead_time_idx}.nc"
+            if not pathlib.Path(climato_path).exists():
+                climato = {}
+                for _,_,_, forecast_idx_in_file, valid_time, forecast_time in self.data_index:
+                    truth = self.obs.sel(time=valid_time)
+                    # compute wind 
+                    if self.target_variable == "10m_wind_speed":
+                        compute_wind_speedxr(truth,"obs")
+
+                    mu = truth[self.target_variable].values.T
+                    lead_time = self.lead_time_idx
+                    date = valid_time.strftime('%m-%d') # get date without year
+                    if date not in climato.keys():
+                        climato[date] = {"mu": [mu]}
+                    else:
+                        climato[date]["mu"].append(mu) 
+
+                # fit gaussians on mu for each date
+                climato_res = []
+                for date in climato.keys():
+                    mu = np.stack(climato[date]["mu"])
+                    mu_fit, sigma_fit = fit_norm_along_axis(mu, axis=0)
+                    mu_fit = mu_fit[np.newaxis, ...]
+                    sigma_fit = sigma_fit[np.newaxis, ...]
+                    ds = xr.Dataset(
+                        data_vars=dict(
+                            mu=(["date", "latitude", "longitude"], mu_fit),
+                            sigma=(["date", "latitude", "longitude"], sigma_fit),
+                        ),
+                        coords=dict(
+                            longitude=("longitude", self.longitude), # 1D array
+                            latitude=("latitude", self.latitude), # 1D array
+                            date=[date] # single item
+                        )
+                    )
+                    climato_res.append(ds)
+                
+                # save to csv
+                final_ds = xr.concat(climato_res, dim='date')
+                final_ds.to_netcdf(climato_path)
+
+        # normalize
+        self.scaler = StandardScaler()
+        self.fit_scaler()
+
+    def fit_scaler(self):
+        """Get all features and fit the scaler on them."""
+        all_features = []
+        for file_surface, file_mean, file_std, forecast_idx_in_file, valid_time, forecast_time in self.data_index:
+            data_mean, data_std = format_ensemble_data_EMOS(file_surface, file_mean, file_std)
+            if self.subset == "test":
+                data_mean = data_mean.rename({'time': 'forecast_time'})
+            data_mean = data_mean.isel(prediction_timedelta=self.lead_time_idx, forecast_time=forecast_idx_in_file)
+            data_mean = data_mean.to_array().values # (n_vars, n_levels, n_lat, n_lon)
+            all_features.append(data_mean)
+
+        all_features = np.concatenate(all_features, axis=0) # Concatenate along the feature dimension
+        all_features = all_features.reshape(all_features.shape[0], -1) # Flatten spatial dimensions
+        self.scaler.fit(all_features)
+
+    def __len__(self):
+        return len(self.data_index)
+
+    def __getitem__(self, idx):
+        file_surface, file_mean, file_std, forecast_idx_in_file, valid_time, forecast_time = self.data_index[idx]
+        data_mean, data_std = format_ensemble_data_EMOS(file_surface, file_mean, file_std)
+        # select forecast data
+        if self.subset == "test":
+            # rename time to forecast_time
+            data_mean = data_mean.rename({'time': 'forecast_time'})
+            data_std = data_std.rename({'time': 'forecast_time'})
+        data_mean = data_mean.isel(prediction_timedelta=self.lead_time_idx, forecast_time=forecast_idx_in_file)
+        data_std = data_std.isel(prediction_timedelta=self.lead_time_idx, forecast_time=forecast_idx_in_file)
+        truth = self.obs.sel(time=valid_time)
+
+        # Normalize features
+        data = data_mean.to_array().values # (n_vars, n_lat, n_lon)
+        shapes = data.shape
+        data = data.reshape(data.shape[0], -1) # Flatten spatial dimensions
+        data = self.scaler.transform(data) # Normalize features
+        data = data.reshape(shapes[0], shapes[1], shapes[2]) # Reshape back
+        features = torch.tensor(data, dtype=torch.float)
+
+        # compute wind 
+        if self.target_variable == "10m_wind_speed":
+            compute_wind_speedxr(truth,"obs")
+
+        # mean std over ensemble predictions for the target variable
+        mu = torch.tensor(data_mean[self.target_variable].values, dtype=torch.float) # (n_lat, n_lon)
+        sigma = torch.tensor(data_std[self.target_variable], dtype=torch.float) # (n_lat, n_lon)
         valid_time = str(valid_time)
         forecast_time = str(forecast_time)
         # get associated ground truth
@@ -280,29 +453,31 @@ if __name__== "__main__":
     #     print(" ")
     #     break
 
-    for variable in ["10m_wind_speed"]:
-        print("Variable",variable)
-        for month in range(1,13):
-            print("Month", month)
-            train_dataset = WeatherDataset(
-                data_path=train_folder,
-                obs_path=obs_folder,
-                target_variable=variable,
-                lead_time_idx=28,
-                valid_years=[1996,2017],
-                valid_months=[month,month],
-                subset="train")
+    # for variable in ["10m_wind_speed"]:
+    #     print("Variable",variable)
+    #     for month in range(1,13):
+    #         print("Month", month)
+    variable = "2m_temperature"
+    month=1
+    train_dataset = WeatherDataset(
+        data_path=train_folder,
+        obs_path=obs_folder,
+        target_variable=variable,
+        lead_time_idx=28,
+        valid_years=[1996,2017],
+        valid_months=[month,month],
+        subset="train")
             #break
         #break
 
-        # train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-        # print("Nb of training examples:",len(train_loader.dataset.data_index))
-        # for batch in train_loader:
-        #     print("SHAPES")
-        #     print(batch['mu'].shape, batch['sigma'].shape, batch['input'].shape, batch['truth'].shape)
-        #     print(batch['forecast_time'], batch['valid_time'], batch['lead_time'])
-        #     print(" ")
-        #     break
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    print("Nb of training examples:",len(train_loader.dataset.data_index))
+    for batch in train_loader:
+        print("SHAPES")
+        print(batch['mu'].shape, batch['sigma'].shape, batch['input'].shape, batch['truth'].shape)
+        print(batch['forecast_time'], batch['valid_time'], batch['lead_time'])
+        print(" ")
+        break
 
     # val_dataset = WeatherDataset(
     # data_path=train_folder,
--- processings/format_data.py
+++ processings/format_data.py
@@ -4,6 +4,7 @@ import xarray as xr
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 from scipy.stats import norm
+import os
 
 def get_year(date: np.datetime64):
     return pd.Timestamp(date).year
@@ -53,3 +54,82 @@ def fit_norm(data):
 def fit_norm_along_axis(arr, axis):
     # Apply the fit_norm function along the specified axis
     return np.apply_along_axis(lambda x: fit_norm(x), axis, arr)
+
+def save_format_ensemble_data_EMOS(surface_dir, levels_dir, out_folder):
+    mean_out_folder = f"{out_folder}/mean"
+    std_out_folder = f"{out_folder}/std"
+
+    for file in os.listdir(surface_dir):
+        if file.endswith(".nc"):
+            dataset_surface = xr.open_dataset(f"{surface_dir}/{file}")
+
+            # mean 
+            mean_surface = dataset_surface.mean(dim="number",skipna=True) # do not take into account nan values
+            mean_levels = xr.open_dataset(f"{levels_dir}/mean/{file}")
+
+            for level in mean_levels.level.values:
+                for var in mean_levels.data_vars:
+                    name = f"{var}_{level}"
+                    mean_surface[name] = mean_levels[var].sel(level=level)
+            
+            mean_surface = mean_surface.drop_vars([f"specific_humidity_{level}" for level in [10,50,100]]) # all NaN
+            mean_surface = mean_surface.drop_vars([f"10m_wind_speed_{level}" for level in mean_levels.level.values[:-1]])
+            
+            # std 
+            std_surface = dataset_surface.std(dim="number",skipna=True) # do not take into account nan values
+            std_levels = xr.open_dataset(f"{levels_dir}/std/{file}")
+
+            for var in std_levels.data_vars:
+                name = f"{var}"
+                std_surface[name] = std_levels[var]
+
+            mean_surface.to_netcdf(f"{mean_out_folder}/{file}")
+            std_surface.to_netcdf(f"{std_out_folder}/{file}")
+
+
+def format_ensemble_data_EMOS(surface_file, levels_mean_file, levels_std_file):
+    dataset_surface = xr.open_dataset(surface_file)
+    # mean 
+    mean_surface = dataset_surface.mean(dim="number",skipna=True) # do not take into account nan values
+    mean_levels = xr.open_dataset(levels_mean_file)
+
+    for level in mean_levels.level.values:
+        for var in mean_levels.data_vars:
+            name = f"{var}_{level}"
+            mean_surface[name] = mean_levels[var].sel(level=level)
+    
+    # drop var
+    mean_surface = mean_surface.drop_vars([f"specific_humidity_{level}" for level in [10,50,100]]) # all NaN
+    mean_surface = mean_surface.drop_vars([f"10m_wind_speed_{level}" for level in mean_levels.level.values[:-1]])
+    # rename 10m_wind_speed_1000 to 10m_wind_speed
+    mean_surface = mean_surface.rename_vars({"10m_wind_speed_1000": "10m_wind_speed"})
+    # nan to zero
+    mean_surface = mean_surface.fillna(0)
+
+    
+    # std 
+    std_surface = dataset_surface.std(dim="number",skipna=True) # do not take into account nan values
+    std_levels = xr.open_dataset(levels_std_file)
+
+    for var in std_levels.data_vars:
+        name = f"{var}"
+        std_surface[name] = std_levels[var]
+
+    # nan to zero
+    std_surface = std_surface.fillna(0)
+    return mean_surface, std_surface
+
+
+if __name__== "__main__":
+    raw_data_folder = "/home/majanvie/scratch/data/raw/"
+    # train
+    surface_dir = f"{raw_data_folder}/train_ensemble_surface"
+    levels_dir = f"{raw_data_folder}/train"
+    out_folder = "/home/majanvie/scratch/data/train/EMOS"
+    format_ensemble_data_EMOS(surface_dir, levels_dir, out_folder)
+
+    # test 
+    surface_dir = f"{raw_data_folder}/test_ensemble_surface"
+    levels_dir = f"{raw_data_folder}/test"
+    out_folder = "/home/majanvie/scratch/data/test/EMOS"
+    format_ensemble_data_EMOS(surface_dir, levels_dir, out_folder)
